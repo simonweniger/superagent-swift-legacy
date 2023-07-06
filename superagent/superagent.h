573a83743152348e386441afb2ff00072c34e264
Simon Weniger <simonweniger.sw@gmail.com>
2023-07-06 11:21:09 +0200

    Initial Commit

---
+++ superagent/superagent.h
@@ -0,0 +1,18 @@
+//
+//  superagent.h
+//  superagent
+//
+//  Created by vonweniger on 06.07.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for superagent.
+FOUNDATION_EXPORT double superagentVersionNumber;
+
+//! Project version string for superagent.
+FOUNDATION_EXPORT const unsigned char superagentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <superagent/PublicHeader.h>
+
+
