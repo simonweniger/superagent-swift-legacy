ef35945d5cbd11872d134ae66716f29679fe7c6c
Simon Weniger <simonweniger.sw@gmail.com>
2023-07-19 18:52:48 +0200

    print prediction input

--- Sources/Superagent/SuperagentSDK.swift
+++ Sources/Superagent/SuperagentSDK.swift
@@ -308,9 +308,9 @@ public struct SuperagentSDK {
 	///Create a new prediction
 	public func createPrediction(agentId: String, prediction: PredictAgent) async throws -> String {
 		//print("create Prediction called")
-		let payload: [String: Any] = ["input": prediction.input as [[String:String]],
+		let payload: [String: Any] = ["input": prediction.input,
 									  "has_Streaming": prediction.hasStreaming as Any]
-		//print("Prediction payload: \(payload)")
+		print("Prediction payload: \(payload)")
 		let data = try await request(method: .post, endpoint: "/agents/\(agentId)/predict", data: payload)
 
 		print("Prediction data:\(data)")
